67e08b368b787b5e3ac18ca8aef3616812e4b271
VictorTrustyDev <me@victortrusty.dev>
2022-06-20 14:43:30 +0700

    update

--- evmos/create-validator.sh
+++ evmos/create-validator.sh
@@ -167,6 +167,20 @@ else
 fi
 echo '- Wait node up'
 sleep 10s
+echo "$EVMOS_BINARY tx staking create-validator \
+	--home="$EVMOS_HOME" \
+	--keyring-backend $KEYRING \
+	--amount="$VAL_STAKE"$MIN_DENOM_SYMBOL \
+	--pubkey=$($BINARY tendermint show-validator --home $EVMOS_HOME) \
+	--moniker="$MONIKER" \
+	--chain-id="$CHAIN_ID" \
+	--commission-rate="$VAL_COMMISSION_RATE" \
+	--commission-max-rate="$VAL_COMMISSION_RATE_MAX" \
+	--commission-max-change-rate="$VAL_COMMISSION_CHANGE_RATE_MAX" \
+	--min-self-delegation="$VAL_MIN_SELF_DELEGATION" \
+	--from="$VAL_KEY_NAME" \
+	--node="tcp://localhost:$DEFAULT_26657" \
+	--yes"
 echo '- Sign & Send message'
 $BINARY tx staking create-validator \
 	--home="$EVMOS_HOME" \
