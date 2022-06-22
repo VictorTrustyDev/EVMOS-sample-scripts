b2ccb45f840be6bf31667563c1a90bc1a6ae64b1
VictorTrustyDev <me@victortrusty.dev>
2022-06-22 23:42:03 +0700

    update

--- evmos-on-docker/cleanup.sh
+++ evmos-on-docker/cleanup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+command -v 'docker-compose' > /dev/null 2>&1 || { echo >&2 "docker-compose is required"; exit 1; }
+
 echo 'docker-compose down'
 docker-compose -f network1.yml down
 docker-compose -f network2.yml down
-
-rm -rf .evmosd*/
-rm -rf EVMOS-source-code-*/
---
+++ hermes-as-ibc-relayer/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+command -v 'docker-compose' > /dev/null 2>&1 || { echo >&2 "docker-compose is required"; exit 1; }
+
+source ../env.sh
+
+if [ $DISABLE_SYSTEMCTL -eq 0 ]; then
+    echo "Stopping $HERMES_SERVICE_NAME service"
+    sudo systemctl stop $HERMES_SERVICE_NAME
+    sudo systemctl disable $HERMES_SERVICE_NAME
+else
+    echo "I don't know what to do"
+fi
