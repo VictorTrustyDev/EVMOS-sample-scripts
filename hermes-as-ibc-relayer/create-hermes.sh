4bee3d1c427f751ceccf2e2d2d3594cc7b9fd9be
VictorTrustyDev <me@victortrusty.dev>
2022-06-24 09:47:55 +0700

    update

--- hermes-as-ibc-relayer/create-hermes.sh
+++ hermes-as-ibc-relayer/create-hermes.sh
@@ -75,12 +75,22 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "s/chain1_account_prefix/$HERMES_CFG_CHAIN_1_ACCOUNT_PREFIX/g" $CONFIG_TOML
     sed -i '' "s/chain1_key_name/$HERMES_CFG_CHAIN_1_KEY_NAME/g" $CONFIG_TOML
     sed -i '' "s/chain1_gas_price_denom/$HERMES_CFG_CHAIN_1_GAS_PRICE_DENOM_SYMBOL/g" $CONFIG_TOML
+    if [ $CHAIN_1_COINTYPE -eq 60 ]; then
+        sed -i '' "s/chain1_address_type/{ derivation = 'ethermint', proto_type = { pk_type = '/ethermint.crypto.v1.ethsecp256k1.PubKey' } }/g" $CONFIG_TOML
+    else
+        sed -i '' "s/chain1_address_type/{ derivation = 'cosmos' }/g" $CONFIG_TOML
+    fi
     sed -i '' "s/chain2_id/$HERMES_CFG_CHAIN_2_ID/g" $CONFIG_TOML
     sed -i '' "s,chain2_rpc_addr,$HERMES_CFG_CHAIN_2_RPC_ADDR,g" $CONFIG_TOML
     sed -i '' "s,chain2_grpc_addr,$HERMES_CFG_CHAIN_2_GRPC_ADDR,g" $CONFIG_TOML
     sed -i '' "s/chain2_account_prefix/$HERMES_CFG_CHAIN_2_ACCOUNT_PREFIX/g" $CONFIG_TOML
     sed -i '' "s/chain2_key_name/$HERMES_CFG_CHAIN_2_KEY_NAME/g" $CONFIG_TOML
     sed -i '' "s/chain2_gas_price_denom/$HERMES_CFG_CHAIN_2_GAS_PRICE_DENOM_SYMBOL/g" $CONFIG_TOML
+    if [ $CHAIN_2_COINTYPE -eq 60 ]; then
+        sed -i '' "s/chain2_address_type/{ derivation = 'ethermint', proto_type = { pk_type = '/ethermint.crypto.v1.ethsecp256k1.PubKey' } }/g" $CONFIG_TOML
+    else
+        sed -i '' "s/chain2_address_type/{ derivation = 'cosmos' }/g" $CONFIG_TOML
+    fi
 else
     sed -i "s/chain1_id/$HERMES_CFG_CHAIN_1_ID/g" $CONFIG_TOML
     sed -i "s,chain1_rpc_addr,$HERMES_CFG_CHAIN_1_RPC_ADDR,g" $CONFIG_TOML
@@ -88,12 +98,22 @@ else
     sed -i "s/chain1_account_prefix/$HERMES_CFG_CHAIN_1_ACCOUNT_PREFIX/g" $CONFIG_TOML
     sed -i "s/chain1_key_name/$HERMES_CFG_CHAIN_1_KEY_NAME/g" $CONFIG_TOML
     sed -i "s/chain1_gas_price_denom/$HERMES_CFG_CHAIN_1_GAS_PRICE_DENOM_SYMBOL/g" $CONFIG_TOML
+    if [ $CHAIN_1_COINTYPE -eq 60 ]; then
+        sed -i "s/chain1_address_type/{ derivation = 'ethermint', proto_type = { pk_type = '/ethermint.crypto.v1.ethsecp256k1.PubKey' } }/g" $CONFIG_TOML
+    else
+        sed -i "s/chain1_address_type/{ derivation = 'cosmos' }/g" $CONFIG_TOML
+    fi
     sed -i "s/chain2_id/$HERMES_CFG_CHAIN_2_ID/g" $CONFIG_TOML
     sed -i "s,chain2_rpc_addr,$HERMES_CFG_CHAIN_2_RPC_ADDR,g" $CONFIG_TOML
     sed -i "s,chain2_grpc_addr,$HERMES_CFG_CHAIN_2_GRPC_ADDR,g" $CONFIG_TOML
     sed -i "s/chain2_account_prefix/$HERMES_CFG_CHAIN_2_ACCOUNT_PREFIX/g" $CONFIG_TOML
     sed -i "s/chain2_key_name/$HERMES_CFG_CHAIN_2_KEY_NAME/g" $CONFIG_TOML
     sed -i "s/chain2_gas_price_denom/$HERMES_CFG_CHAIN_2_GAS_PRICE_DENOM_SYMBOL/g" $CONFIG_TOML
+    if [ $CHAIN_2_COINTYPE -eq 60 ]; then
+        sed -i "s/chain2_address_type/{ derivation = 'ethermint', proto_type = { pk_type = '/ethermint.crypto.v1.ethsecp256k1.PubKey' } }/g" $CONFIG_TOML
+    else
+        sed -i "s/chain2_address_type/{ derivation = 'cosmos' }/g" $CONFIG_TOML
+    fi
 fi
 
 # Binary
