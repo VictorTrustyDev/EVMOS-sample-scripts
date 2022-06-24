74b2d743328708583d7f7259932c20dca8b7a987
Victor Pham <me@victortrusty.dev>
2022-06-24 13:43:31 +0700

    Update script AIO (#13)

--- all-in-one.sh
+++ all-in-one.sh
@@ -60,6 +60,9 @@ else
     show_required_tools 'hasura-cli'
     exit 1
 fi
+if [ -f "./override-env.sh" ]; then
+    source "./override-env.sh"
+fi
 
 echo "Run everything requires:"
 echo "- Your machine must have at least 4 Cores and 16 Gb of RAM"
@@ -132,10 +135,19 @@ if [ -f "./override-env.sh" ]; then
     source "./override-env.sh"
 fi
 echo "> [Load up token for IBC account on network 1]"
-docker exec -it vtevmos11 bash -c "evmosd tx bank send $VAL_2_KEY_NAME $REL_1_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_1_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_1_GAS_PRICE_DENOM_SYMBOL --home /.evmosd11 --yes"
+echo "Keyring: $KEYRING"
+if [ "$KEYRING" = "test" ]; then
+    docker exec -it vtevmos11 bash -c "evmosd tx bank send $VAL_2_KEY_NAME $REL_1_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_1_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_1_GAS_PRICE_DENOM_SYMBOL --home /.evmosd11 --yes"
+else
+    docker exec -it vtevmos11 bash -c "echo '$VAL_KEYRING_FILE_ENCRYPTION_PASSWORD' | evmosd tx bank send $VAL_2_KEY_NAME $REL_1_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_1_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_1_GAS_PRICE_DENOM_SYMBOL --home /.evmosd11 --yes"
+fi
 [ $? -eq 0 ] || { echo >&2 "ERR AIO: Operation failed"; exit 1; }
 echo "> [Load up token for IBC account on network 2]"
-docker exec -it vtevmos21 bash -c "evmosd tx bank send $VAL_2_KEY_NAME $REL_2_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_2_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_2_GAS_PRICE_DENOM_SYMBOL --home /.evmosd21 --yes"
+if [ "$KEYRING" = "test" ]; then
+    docker exec -it vtevmos21 bash -c "evmosd tx bank send $VAL_2_KEY_NAME $REL_2_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_2_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_2_GAS_PRICE_DENOM_SYMBOL --home /.evmosd21 --yes"
+else
+    docker exec -it vtevmos21 bash -c "echo '$VAL_KEYRING_FILE_ENCRYPTION_PASSWORD' | evmosd tx bank send $VAL_2_KEY_NAME $REL_2_ADDR $(bc <<< "$HERMES_RESERVED_FEE * (10^$HERMES_CFG_CHAIN_2_DENOM_EXPONENT)")$HERMES_CFG_CHAIN_2_GAS_PRICE_DENOM_SYMBOL --home /.evmosd21 --yes"
+fi
 [ $? -eq 0 ] || { echo >&2 "ERR AIO: Operation failed"; exit 1; }
 
 echo "> [Hermes]"
--- evmos-on-docker/README.md
+++ evmos-on-docker/README.md
@@ -36,18 +36,21 @@ Now you can [follow this sample](https://github.com/VictorTrustyDev/EVMOS-sample
         + 50m EVMOS
         + 7k coin staked
         + Addr: evmos1wuqvcpuunf7r5rg7xutqddhw55grfzc75qejyq
+        + Private key: FC3F58B007A017166DE5A340C7A2641EB37CF37081D6F9013636CEBFBAF7B1FE
         + Mnemonic: spoil senior door access upset floor decorate shield high punch senior tape pigeon base slogan height clever buffalo cat report poem weapon labor satoshi
         + Key name: val1
     + evmosvaloper1zxgt4pwzzsv02z24g80lc5rhtsp0prw046yxss
         + 50m EVMOS
         + 3k coin staked
         + Addr: evmos1zxgt4pwzzsv02z24g80lc5rhtsp0prw0c5tk3d
+        + Private key: 0172DC491B5ACD04DD378D3FD8FD9F41A0D701E070941474FADECD72E1E085B9
         + Mnemonic: width produce brush hour horse retreat play flag fresh broken measure culture scare broken erupt pilot buzz embody depend topic behind rigid fan battle
         + Key name: val2
     + evmosvaloper1vcy9v4jp0sd4hysqqcuwleytxre3ms4cmv5ajl
         + 50m EVMOS
         + 3k coin staked
         + Addr: evmos1vcy9v4jp0sd4hysqqcuwleytxre3ms4ckzmdnz
+        + Private key: E0D83C6054597638469CC91A46F14B7F62705297912524059629E4674302928F
         + Mnemonic: stage grid emotion thumb safe myth chair dizzy beyond casual select polar hover retire master neglect shift zero trigger section token replace truly father
         + Key name: val3
 
