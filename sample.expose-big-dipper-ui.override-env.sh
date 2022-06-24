4b0f87bed46d60c32236a5f4fe5719c52a800b8f
VictorTrustyDev <me@victortrusty.dev>
2022-06-25 02:07:27 +0700

    update

--- sample.expose-big-dipper-ui.override-env.sh
+++ sample.expose-big-dipper-ui.override-env.sh
@@ -2,8 +2,8 @@
 
 # You can rename this file into `override-env.sh` to make the block explorer become accessible from outside world
 
-export BD2_CFG_CHAIN_1_PUBLIC_DOMAIN="your.domain.example.com"
-export BD2_CFG_CHAIN_1_PUBLIC_RPC_26657="your.domain.example.com:$EVMOS_CHAIN_1_PORT_RPC"
+export BD2_CFG_CHAIN_1_PUBLIC_DOMAIN="bigdipper.example.com"
+export BD2_CFG_CHAIN_1_PUBLIC_RPC_26657="rpc.example.com:$EVMOS_CHAIN_1_PORT_RPC"
 
-export BD2_CFG_CHAIN_2_PUBLIC_DOMAIN="your.domain.example.com"
-export BD2_CFG_CHAIN_2_PUBLIC_RPC_26657="your.domain.example.com:$EVMOS_CHAIN_2_PORT_RPC"
+export BD2_CFG_CHAIN_2_PUBLIC_DOMAIN="bigdipper.example.com"
+export BD2_CFG_CHAIN_2_PUBLIC_RPC_26657="rpc.example.com:$EVMOS_CHAIN_2_PORT_RPC"
